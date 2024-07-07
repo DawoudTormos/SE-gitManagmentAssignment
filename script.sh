b13e951407ed98f7f3ccb8b4bfa1540d50d44078
DawoudTormos <103008170+DawoudTormos@users.noreply.github.com>
2024-07-08 00:51:54 +0300

    Adding main script for the solution

---
+++ script.sh
@@ -0,0 +1,16 @@
+# Fetch changes in the main project
+git fetch
+
+echo -e "\nAutomatically updating only submodules to their latest commits. Then commiting and pushing this change.\n\n"
+
+# Loop through each submodule and check for updates
+git submodule foreach 'git fetch && git status && git pull origin $(git rev-parse --abbrev-ref HEAD) && echo -e "\n---------------\n"'
+
+# Stage changes in submodules only
+git submodule foreach 'git add .'
+
+# Commit submodule updates in the main project
+git commit -m "Automatically updating submodules to their latest commits."
+
+# Push changes to GiytHub
+git push
